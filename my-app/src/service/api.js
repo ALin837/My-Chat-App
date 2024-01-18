729c8ae165ddb5d8fb07a50490af9a6c7ad3af54
Andrew Lin <77423271+ALin837@users.noreply.github.com>
2024-01-17 21:06:26 -0500

    Define API URL

--- my-app/src/service/api.js
+++ my-app/src/service/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
+const baseURLinstance = process.env.API_URL || "http://localhost:9000/";
+
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:9000/',
+    baseURL: baseURLinstance,
     headers: { 
         'Content-Type': 'application/json' 
     },
