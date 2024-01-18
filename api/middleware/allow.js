9f9cc8dcc3dde099bcc1a4f77a2b3c13f6b3c234
Andrew Lin <77423271+ALin837@users.noreply.github.com>
2024-01-18 17:17:24 -0500

    puit headers in vercel

--- api/middleware/allow.js
+++ api/middleware/allow.js
@@ -4,12 +4,12 @@ const check = (req, res, next) => {
     const origin = req.headers['origin'];
     if (allowedOrigins.includes(origin)) {
         res.header('Access-Control-Allow-Credentials', true);
-        res.header('Access-Control-Allow-Origin', origin);
-        res.header('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-        res.header(
-          'Access-Control-Allow-Headers',
-          'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-        )
+        //res.header('Access-Control-Allow-Origin', origin);
+        //res.header('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+        //res.header(
+        //  'Access-Control-Allow-Headers',
+        //  'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+        //)
     }
     next()
 }
