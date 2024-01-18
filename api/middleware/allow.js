485af1895a75529891144902e19384f49b9a8f81
Andrew Lin <77423271+ALin837@users.noreply.github.com>
2024-01-17 23:50:03 -0500

    allow

--- api/middleware/allow.js
+++ api/middleware/allow.js
@@ -4,7 +4,7 @@ const check = (req, res, next) => {
     const origin = req.headers['origin'];
     if (allowedOrigins.includes(origin)) {
         res.header('Access-Control-Allow-Credentials', true);
-        res.header('Access-Control-Allow-Origin', origin);
+        res.header('Access-Control-Allow-Origin', '*');
     }
     next()
 }
