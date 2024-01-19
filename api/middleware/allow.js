9185f9239e335a1cdbe183654234c9993eebd73d
Andrew Lin <77423271+ALin837@users.noreply.github.com>
2024-01-19 00:33:05 -0500

    Try max age

--- api/middleware/allow.js
+++ api/middleware/allow.js
@@ -5,6 +5,7 @@ const check = (req, res, next) => {
     if (allowedOrigins.includes(origin)) {
         res.header('Access-Control-Allow-Credentials', true);
         res.header('Access-Control-Allow-Origin', origin);
+        res.header('Access-Control-Max-Age', 86400);
     }
     next()
 }
