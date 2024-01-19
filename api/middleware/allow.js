db070fbfad14864b23fa555eb07e1c5af0b27c59
Andrew Lin <77423271+ALin837@users.noreply.github.com>
2024-01-19 08:58:08 -0500

    set headers

--- api/middleware/allow.js
+++ api/middleware/allow.js
@@ -6,6 +6,7 @@ const check = (req, res, next) => {
         res.setHeader('Access-Control-Allow-Credentials', true);
         res.setHeader('Access-Control-Allow-Origin', origin);
         res.setHeader('Access-Control-Max-Age', 86400);
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type");
          // Handle preflight requests
         if (req.method === 'OPTIONS') {
             res.status(200).end();
