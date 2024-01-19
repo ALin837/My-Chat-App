57d03c5e371fdece9d7b9f6c5e06c704fdca69ad
Andrew Lin <77423271+ALin837@users.noreply.github.com>
2024-01-19 01:06:25 -0500

    path

--- api/config/allowed.js
+++ api/config/allowed.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     //'http://localhost:3000',
-    'https://mychatapp-xqyr.onrender.com'
+    'https://mychatapp-xqyr.onrender.com/'
 ]
 
 module.exports = allowedOrigins;
