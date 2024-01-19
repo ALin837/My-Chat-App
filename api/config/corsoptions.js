b31e2526e54fec103a001ab18f54e8284dc3e850
Andrew Lin <77423271+ALin837@users.noreply.github.com>
2024-01-19 08:31:26 -0500

    try this

--- api/config/corsoptions.js
+++ api/config/corsoptions.js
@@ -1,14 +1,6 @@
 const allowedOrigins = require("./allowed")
 const corsOptions  = {
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            return callback(null, true);
-        } else {
-            var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-
-    }, 
+    origin: allowedOrigins,
     credentials: true,
     optionsSuccessStatus: 200
 }
