5615e7d9602b36f5b5a37c80754efe1f9d70a39d
Andrew Lin <77423271+ALin837@users.noreply.github.com>
2024-01-19 08:48:38 -0500

    add to all routes

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,6 +3,10 @@ const dbo = require('../utils/conn')
 var router = express.Router();
 
 router.get('/all', async (req, response) => {
+  response.setHeader("Access-Control-Allow-Origin", 'https://mychatapp-xqyr.onrender.com')
+  response.setHeader("Access-Control-Allow-Credentials", "true");
+  response.setHeader("Access-Control-Max-Age", "1800");
+  response.setHeader("Access-Control-Allow-Headers", "content-type");
     const dbConnect = dbo.getDb();  
     try {
       const user = await dbConnect.collection("user").find({}).project( {username : 1, _id: 1}).toArray();
@@ -19,6 +23,10 @@ router.get('/all', async (req, response) => {
 
 
 router.get('/:userId', async (req, response) => {
+  response.setHeader("Access-Control-Allow-Origin", 'https://mychatapp-xqyr.onrender.com')
+  response.setHeader("Access-Control-Allow-Credentials", "true");
+  response.setHeader("Access-Control-Max-Age", "1800");
+  response.setHeader("Access-Control-Allow-Headers", "content-type");
     const dbConnect = dbo.getDb();  
     try {
       const user = await dbConnect.collection("user").find({_id: req.params.userId});
